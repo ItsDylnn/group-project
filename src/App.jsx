5cdf4bec625d9dd55441f2234c4aa4c79ad28c65
George-alt5 <georgekihoro6@gmail.com>
2025-07-25 08:03:06 +0300

    import App.css and apply consistent orange-black theme across UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
+import './App.css'
 
-function App(){
-  return(
+function App() {
+  return (
     <Router>
       <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/cars/:id" element = {<CarDetails/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
       </Routes>
     </Router>
-  )
+  );
 }
-export default App();
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import "./App.ccs";
+import "./index.css"; // your global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
-
-)
+);
